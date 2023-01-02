95375a4cc71008fb9a7485796e9b2fe2acce0f1a
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-01-02 14:42:23 +0000

    added gameboard

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let gameboard = []
+const imgX = document.createElement('img')
+imgX.src = './images/Picture2.png'
+const imgO = document.createElement('img')
+imgO.src = './images/Picture1.png'
 
 class Player {
     constructor(name) {
         this.name = name
     }
 }
+
+const createBoard = () => { }
